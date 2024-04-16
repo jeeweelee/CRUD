507971f412079a60e9f7500685c1356b2679c3cc
Henry Rothenberg <hsr205@aol.com>
2024-04-16 18:02:25 -0400

    Remove / Add dependancies / mapping logic

--- src/main/java/com/mycompany/department/Department.java
+++ src/main/java/com/mycompany/department/Department.java
@@ -1,12 +1,16 @@
 package com.mycompany.department;
 
+import com.mycompany.professor.Professor;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -21,5 +25,8 @@ public class Department {
 
     @Column(length = 100, nullable = false, name = "department_location")
     private String department_location;
+
+    @OneToMany(mappedBy = "department")
+    private Set<Professor> professors;
 
 }
