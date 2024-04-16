3922a38211fe26af1fb811b34c01a80ecc83e696
Henry Rothenberg <hsr205@aol.com>
2024-04-16 18:05:54 -0400

    Alter / Add / Remove logic

--- src/main/java/com/mycompany/classes/Classes.java
+++ src/main/java/com/mycompany/classes/Classes.java
@@ -1,12 +1,16 @@
 package com.mycompany.classes;
 
+import com.mycompany.professor.Professor;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -24,5 +28,8 @@ public class Classes {
 
     @Column(length = 5, nullable = false, name = "num_students")
     private Integer num_students;
+
+    @OneToMany(mappedBy = "classes")
+    private Set<Professor> professors;
 
 }
