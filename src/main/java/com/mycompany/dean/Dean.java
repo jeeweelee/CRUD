258d48191c64dc61ad73d2a9cf43e0f290734636
Henry Rothenberg <hsr205@aol.com>
2024-04-16 18:04:42 -0400

    Alter / Add / Remove logic

--- src/main/java/com/mycompany/dean/Dean.java
+++ src/main/java/com/mycompany/dean/Dean.java
@@ -1,12 +1,16 @@
 package com.mycompany.dean;
 
+import com.mycompany.professor.Professor;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -16,17 +20,18 @@ public class Dean {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, unique = true, length = 45)
+    @Column(unique = true, length = 45)
     private String email;
 
-    @Column(length = 15, nullable = false)
+    @Column(length = 15)
     private String password;
 
-    @Column(length = 45, nullable = false, name = "first_name")
+    @Column(length = 45, name = "first_name")
     private String firstName;
 
-    @Column(length = 45, nullable = false, name = "last_name")
+    @Column(length = 45, name = "last_name")
     private String lastName;
 
-    private boolean enabled;
+    @OneToMany(mappedBy = "dean")
+    private Set<Professor> professors;
 }
